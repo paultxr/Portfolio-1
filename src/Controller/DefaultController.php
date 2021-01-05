bfcd55990d27e1d38e2c3b4b701a50c044e96664
julien willette <julien.willette@gmail.com>
2021-01-05 17:34:59 +0100

    add easyAdmin add new database add project controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'website' => 'Porte Folio',
+            'website' => 'Portfolio',
         ]);
     }
 }
