90160cb24caea19e7e79d7ee5a1e08da222e7e3a
julien willette <julien.willette@gmail.com>
2021-01-05 17:12:39 +0100

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/NationController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     *  @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'website' => 'Porte Folio',
+        ]);
+    }
+}
